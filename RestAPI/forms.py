61bf136bd6b11850c4eac9d5aa92acc736e24858
Hajcik <hajcik666pl@gmail.com>
2022-01-13 21:07:22 +0100

    filtering added

--- RestAPI/forms.py
+++ RestAPI/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 
+from RestAPI.models import Twitt
+
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
@@ -10,3 +12,8 @@ class LoginForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username','password']
+
+class TwittForm(forms.ModelForm):
+    class Meta:
+        model = Twitt
+        fields = ['title', 'content', 'autor']
